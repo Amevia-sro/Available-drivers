85e4916abf57be8a26242bc0e0b613d0147ea710
Amevia <66975640+Amevia-sro@users.noreply.github.com>
2024-01-16 10:57:28 +0100

    Add files via upload

---
+++ available_drivers/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Available Drivers",
+    'version': '16.0.1.0.0',
+    'category': 'Project',
+       'summary': """Get all available drivers for the task at given time and date""",
+    'description': """This module helps to find out the available drivers for a
+     project task based on the task start,end date and time.""",
+    'author': 'AMV',
+    'company': 'Amevia s.r.o.',
+    'maintainer': 'AMV',
+    'website': "www.amevia.eu",
+    'depends': ['project', 'hr_timesheet'],
+    'data': [
+    'security/ir.model.access.csv',
+    'views/project_task_views.xml',
+    'views/res_users_views.xml',
+    'wizards/available_drivers_views.xml'  
+],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
---
+++ available_drivers/models/project_task.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    Journey_start_date = fields.Datetime(string="Journey Start Date", required=True, help='Start date and time of the journey')
+    Journey_end_date = fields.Datetime(string="Journey End Date", required=True, help='End date and time of the journey')
+    users_ids = fields.Many2many('res.users', compute="_compute_users_ids", help="Users available for the task")
+
+    @api.depends('Journey_start_date', 'Journey_end_date', 'project_id.message_follower_ids')
+    def _compute_users_ids(self):
+        for record in self:
+            if record.Journey_end_date and record.Journey_start_date:
+                start_date = record.Journey_start_date
+                end_date = record.Journey_end_date
+                driver_ids = self.get_available_driver_ids(start_date, end_date)
+                domain = [('id', 'not in', driver_ids), ('share', '=', False)]
+                if record.project_id.privacy_visibility == 'followers':
+                    domain.append(('id', 'in', record.project_id.message_follower_ids.ids))
+                record.users_ids = self.env['res.users'].search(domain)
+
+    def get_available_driver_ids(self, start_date, end_date):
+        """ Get the available drivers for the particular period """
+        driver_ids = []
+        tasks = self.env['project.task'].search([
+            '|',
+            '&', ('Journey_start_date', '<=', start_date), ('Journey_end_date', '>=', start_date),
+            '&', ('Journey_start_date', '<=', end_date), ('Journey_end_date', '>=', end_date)
+        ])
+        for task in tasks:
+            if task.manager_id:
+                driver_ids.append(task.manager_id.id)
+            driver_ids.extend(task.user_ids.ids)
+        return list(set(driver_ids))
+
